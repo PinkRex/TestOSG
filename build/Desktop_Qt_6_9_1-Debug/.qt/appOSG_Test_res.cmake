470963e17e33fdc3cefb332bf12120d8c33451ea
PinkRex <ducnguyenminh1912003@gmail.com>
2025-08-04 16:33:24 +0700

    add 3D Buildings

--- build/Desktop_Qt_6_9_1-Debug/.qt/appOSG_Test_res.cmake
+++ build/Desktop_Qt_6_9_1-Debug/.qt/appOSG_Test_res.cmake
@@ -2,8 +2,8 @@
 set(target "appOSG_Test")
 set(working_dir "/home/canhdx/OSG_Test")
 set(src_and_dest_list
-    "/home/canhdx/OSG_Test/Test.earth"
-    "/home/canhdx/OSG_Test/build/Desktop_Qt_6_9_1-Debug/OSG_Test/Test.earth"
+    "/home/canhdx/OSG_Test/Note.txt"
+    "/home/canhdx/OSG_Test/build/Desktop_Qt_6_9_1-Debug/OSG_Test/Note.txt"
 
 )
 set(timestamp_file "/home/canhdx/OSG_Test/build/Desktop_Qt_6_9_1-Debug/.qt/appOSG_Test_res.txt")
--- osgearthrenderer2.cpp
+++ osgearthrenderer2.cpp
@@ -1,7 +1,11 @@
 #include "osgearthrenderer2.h"
 #include <osgEarth/EarthManipulator>
 #include <osgEarth/LogarithmicDepthBuffer>
+#include <osgEarth/ImageLayer>
 #include <osgEarth/TMS>
+#include <osgEarth/FeatureModelLayer>
+#include <osgEarth/OGRFeatureSource>
+
 #include <osgViewer/ViewerEventHandlers>
 #include <QOpenGLFramebufferObject>
 
@@ -29,6 +33,9 @@ void OsgEarthRenderer2::initOsgEarthScene() {
     m_map = new osgEarth::Map;
     addImagery();
     addElevation();
+    addBuildings();
+    addStreets();
+    addParks();
 
     m_mapNode = new osgEarth::MapNode(m_map.get());
     osg::Group* root = new osg::Group;
@@ -57,15 +64,140 @@ void OsgEarthRenderer2::addElevation() {
 }
 
 void OsgEarthRenderer2::addBuildings() {
+    osgEarth::OGRFeatureSource* data = new osgEarth::OGRFeatureSource();
+    data->setName("buildings-data");
+    data->setURL(BUILDINGS_URL);
 
+    qDebug() << data->getName();
+    qDebug() << QString::fromStdString(data->getURL().full());
+
+    auto status = data->open();
+    if (!status.isOK()) {
+        qDebug() << "Open failed:" << QString::fromStdString(status.message());
+    }
+
+
+    osgEarth::Style buildingStyle;
+    buildingStyle.setName( "default" );
+
+    osgEarth::ExtrusionSymbol* extrusion = buildingStyle.getOrCreate<osgEarth::ExtrusionSymbol>();
+    extrusion->heightExpression() = osgEarth::NumericExpression( "3.5 * max( [story_ht_], 1 )" );
+    extrusion->flatten() = true;
+    extrusion->wallStyleName() = "building-wall";
+    extrusion->roofStyleName() = "building-roof";
+
+    osgEarth::PolygonSymbol* poly = buildingStyle.getOrCreate<osgEarth::PolygonSymbol>();
+    poly->fill()->color() = osgEarth::Color::White;
+
+    osgEarth::AltitudeSymbol* alt = buildingStyle.getOrCreate<osgEarth::AltitudeSymbol>();
+    alt->clamping() = alt->CLAMP_TO_TERRAIN;
+    alt->binding() = alt->BINDING_VERTEX;
+
+    osgEarth::Style wallStyle;
+    wallStyle.setName( "building-wall" );
+    osgEarth::SkinSymbol* wallSkin = wallStyle.getOrCreate<osgEarth::SkinSymbol>();
+    wallSkin->library() = "us_resources";
+    wallSkin->addTag( "building" );
+    wallSkin->randomSeed() = 1;
+
+    osgEarth::Style roofStyle;
+    roofStyle.setName( "building-roof" );
+    osgEarth::SkinSymbol* roofSkin = roofStyle.getOrCreate<osgEarth::SkinSymbol>();
+    roofSkin->library() = "us_resources";
+    roofSkin->addTag( "rooftop" );
+    roofSkin->randomSeed() = 1;
+    roofSkin->isTiled() = true;
+
+    osgEarth::StyleSheet* styleSheet = new osgEarth::StyleSheet();
+    styleSheet->addStyle( buildingStyle );
+    styleSheet->addStyle( wallStyle );
+    styleSheet->addStyle( roofStyle );
+
+    osgEarth::ResourceLibrary* reslib = new osgEarth::ResourceLibrary( "us_resources", RESOURCE_LIB_URL );
+    styleSheet->addResourceLibrary( reslib );
+
+    osgEarth::FeatureDisplayLayout layout;
+    layout.tileSize() = 500;
+
+    osgEarth::FeatureModelLayer* layer = new osgEarth::FeatureModelLayer();
+    layer->setName("Buildings");
+    layer->setFeatureSource(data);
+    layer->setStyleSheet(styleSheet);
+    layer->setLayout(layout);
+    layer->setMaxVisibleRange(20000.0);
+
+    m_map->addLayer(layer);
 }
 
 void OsgEarthRenderer2::addStreets() {
+    osgEarth::OGRFeatureSource* data = new osgEarth::OGRFeatureSource();
+    data->setURL(STREETS_URL);
+    data->options().buildSpatialIndex() = true;
 
+    osgEarth::ResampleFilterOptions resample;
+    resample.minLength() = 0.0f;
+    resample.maxLength() = 25.0f;
+    data->options().filters().push_back( resample );
+
+    osgEarth::Style style;
+    style.setName( "streets" );
+
+    osgEarth::LineSymbol* line = style.getOrCreate<osgEarth::LineSymbol>();
+    line->stroke()->color() = osgEarth::Color(osgEarth::Color::Yellow, 0.5f);
+    line->stroke()->width() = 7.5f;
+    line->stroke()->widthUnits() = osgEarth::Units::METERS;
+
+    osgEarth::AltitudeSymbol* alt = style.getOrCreate<osgEarth::AltitudeSymbol>();
+    alt->clamping() = alt->CLAMP_TO_TERRAIN;
+
+    osgEarth::RenderSymbol* render = style.getOrCreate<osgEarth::RenderSymbol>();
+    render->depthOffset()->minBias()->set(6.6, osgEarth::Units::METERS);
+
+    osgEarth::FeatureDisplayLayout layout;
+    layout.tileSize() = 500;
+
+    osgEarth::FeatureModelLayer* layer = new osgEarth::FeatureModelLayer();
+    layer->setName("Streets");
+    layer->setFeatureSource(data);
+    layer->options().layout() = layout;
+    layer->setStyleSheet(new osgEarth::StyleSheet());
+    layer->getStyleSheet()->addStyle(style);
+    layer->setMaxVisibleRange(5000.0f);
+
+    m_map->addLayer(layer);
 }
 
 void OsgEarthRenderer2::addParks() {
+    osgEarth::OGRFeatureSource* data = new osgEarth::OGRFeatureSource();
+    data->setURL(PARKS_URL);
+    data->options().buildSpatialIndex() = true;
 
+    osgEarth::Style style;
+    style.setName( "parks" );
+
+    osgEarth::ModelSymbol* model = style.getOrCreate<osgEarth::ModelSymbol>();
+    model->url()->setLiteral(TREE_MODEL_URL);
+    model->placement() = model->PLACEMENT_RANDOM;
+    model->density() = 6000.0f;
+
+    osgEarth::AltitudeSymbol* alt = style.getOrCreate<osgEarth::AltitudeSymbol>();
+    alt->clamping() = alt->CLAMP_TO_TERRAIN;
+
+    osgEarth::RenderSymbol* render = style.getOrCreate<osgEarth::RenderSymbol>();
+    render->transparent() = true;
+    render->minAlpha() = 0.15f;
+
+    osgEarth::FeatureDisplayLayout layout;
+    layout.tileSize() = 650;
+    layout.addLevel(osgEarth::FeatureLevel(0.0f, 2000.0f, "parks"));
+
+    osgEarth::FeatureModelLayer* layer = new osgEarth::FeatureModelLayer();
+    layer->setFeatureSource(data);
+    layer->options().layout() = layout;
+    layer->setStyleSheet(new osgEarth::StyleSheet());
+    layer->getStyleSheet()->addStyle(style);
+
+    m_map->addLayer(layer);
 }
 
 QOpenGLFramebufferObject *OsgEarthRenderer2::createFramebufferObject(const QSize &size) {
