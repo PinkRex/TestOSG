98fb3dd9fe767046a3df2ed2508c7f3b5330cf6d
PinkRex <ducnguyenminh1912003@gmail.com>
2025-08-13 09:11:41 +0700

    Remove Secret

--- build/Desktop_Qt_6_9_1-Debug/.qt/appOSG_Test_res.cmake
+++ build/Desktop_Qt_6_9_1-Debug/.qt/appOSG_Test_res.cmake
@@ -2,8 +2,6 @@
 set(target "appOSG_Test")
 set(working_dir "/home/canhdx/TestOSG")
 set(src_and_dest_list
-    "/home/canhdx/TestOSG/Note.txt"
-    "/home/canhdx/TestOSG/build/Desktop_Qt_6_9_1-Debug/OSG_Test/Note.txt"
 
 )
 set(timestamp_file "/home/canhdx/TestOSG/build/Desktop_Qt_6_9_1-Debug/.qt/appOSG_Test_res.txt")
